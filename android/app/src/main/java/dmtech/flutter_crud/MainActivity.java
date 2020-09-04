e085b1e09d483bf0931892e33e992390d801c0c1
Dimas Mikael <dimasmikael.souza@gmail.com>
2020-09-04 09:38:02 -0300

    Projeto flutter_CRUD

---
+++ android/app/src/main/java/dmtech/flutter_crud/MainActivity.java
@@ -0,0 +1,6 @@
+package dmtech.flutter_crud;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/data/dammy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  "1": const User (
+      id: "1",
+      name : 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarURL : 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914__340.png'
+  ),
+  "2": const User (
+      id: "2",
+      name : 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarURL : 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331__340.png'
+  ),
+  "3": const User (
+      id: "3",
+      name : 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarURL : 'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510__340.png'
+  ),
+  "4": const User (
+      id: "4",
+      name : 'Guilherme',
+      email: 'guigui@abmail.com',
+      avatarURL : 'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425__340.png'
+  ),
+};
